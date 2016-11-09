abe3e7b0b30d997cb71ed513d1fc9afe762de00f
houg <hougang_ahut@163.com>
2016-11-09 16:30:18 +0800

    js

--- src/main/webapp/resources/spm_modules/app/jsp/order/jsviews-order.js
+++ src/main/webapp/resources/spm_modules/app/jsp/order/jsviews-order.js
@@ -3,27 +3,6 @@ define(function (require, exports, module) {
 	require("jsviews/jsrender");
 	require("jsviews/jsviews");
 	
-	//订单后厂状态
-	var stateMap = new jMap();
-	stateMap.put("10", "提交");
-	stateMap.put("11", "待支付");
-	stateMap.put("12", "已支付");
-	stateMap.put("13", "待报价");
-	stateMap.put("20", "待领取");
-	stateMap.put("21", "已领取");
-	stateMap.put("211", "已分配");
-	stateMap.put("23", "翻译中");
-	stateMap.put("24", "已提交翻译");
-	stateMap.put("25", "修改中");
-	stateMap.put("40", "待审核");
-	stateMap.put("41", "已审核");
-	stateMap.put("42", "审核不通过");
-	stateMap.put("51", "已确认");
-	stateMap.put("52", "待评价");
-	stateMap.put("53", "已评价");
-	stateMap.put("90", "完成");
-	stateMap.put("91", "已取消");
-	stateMap.put("92", "已退款");
 	
 	//订单来源
 	var chlIdMap = new jMap();
@@ -53,6 +32,72 @@ define(function (require, exports, module) {
 	payStyleMap.put("7", "优惠券");
 	payStyleMap.put("11", "银行汇款/转账");
 	
+	//订单后厂状态
+	var stateMap = new jMap();
+	stateMap.put("10", "提交");
+	stateMap.put("11", "待支付");
+	stateMap.put("12", "已支付");
+	stateMap.put("13", "待报价");
+	stateMap.put("20", "待领取");
+	stateMap.put("21", "已领取");
+	stateMap.put("211", "已分配");
+	stateMap.put("23", "翻译中");
+	stateMap.put("24", "已提交翻译");
+	stateMap.put("25", "修改中");
+	stateMap.put("40", "待审核");
+	stateMap.put("41", "已审核");
+	stateMap.put("42", "审核不通过");
+	stateMap.put("51", "已确认");
+	stateMap.put("52", "待评价");
+	stateMap.put("53", "已评价");
+	stateMap.put("90", "完成");
+	stateMap.put("91", "已取消");
+	stateMap.put("92", "已退款");
+	
+	/*
+	 * 获取订单操作日志操作名称
+	 */
+	$.views.helpers({
+		"getOrderOperName": function (subFlag,orgState,newState){	
+			if(orgState==newState){
+	        	return "修改订单";
+	        }
+			//系统自动报价
+			if(subFlag==0){
+				if(!newState||newState=='10'||newState=='11'){
+					return "提交订单";
+				}
+			}else{
+				if(!newState||newState=='10'||newState=='13'){
+					return "提交订单";
+				}
+				if(newState=='11'){
+					return "手动报价";
+				}
+			}
+	        if(newState=='20'){
+	        	return "支付订单";
+	        }
+	        if(newState=='21'){
+	        	return "领取订单";
+	        }
+	        if(newState=='211'){
+	        	return "分配订单";
+	        }
+	        return newState;
+		 }
+	});
+	
+	$.views.helpers({
+		"getMoneyUnit": function (currencyUnit){	
+			if(currencyUnit=='2'){
+	        	return "美元";
+	        }else{
+	        	return "元";
+	        }
+		 }
+	});
+	
 	/**
 	 * 获取订单后厂状态名称
 	 */
@@ -71,6 +116,8 @@ define(function (require, exports, module) {
 	     }
 	     
 	});
+	
+	
 	
 	function jMap(){
         //私有变量
